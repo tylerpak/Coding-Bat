9b4748ebb779ebd330461baf6d0f675416a97d95
tylerpak <paktyler@utexas.edu>
2025-08-01 00:32:50 -0500

    Completed equalIsNot.
    Basically used two iterations to scrub the string once to count "is", and a second time to count "not". After the two iterations I compared the two counts.

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.println(equalIsNot("This is not not"));
+        System.out.println(equalIsNot("This is not"));
     }
 
     public static boolean equalIsNot(String str) {
@@ -11,14 +11,15 @@ public class Main {
         int isCount = 0;
         int notCount = 0;
 
-        for(int i = 0; i < str.length() - is.length(); i++) {
+        for(int i = 0; i <= str.length() - is.length(); i++) {
             if(str.substring(i, i+is.length()).equals("is")) {
                 isCount ++;
             }
         }
 
-        for(int i = 0; i < str.length() - not.length(); i++) {
-            if(str.substring(i, i + not.length()).equals("not")) {
+        for(int i = 0; i <= str.length() - not.length(); i++) {
+            String sub = str.substring(i, i + not.length());
+            if(sub.equals("not")) {
                 notCount++;
             }
         }
